231e2603a600986787a1d4464085176fa9b8a966
Sergii Lovygin <sergey.lovygin@yahoo.com>
2022-04-11 15:57:09 +0300

    add alpha version of solution
    Currently, Tree._create_tree_data method returns incorrect data,
    but shows the general idea

--- solution.py
+++ solution.py
@@ -1,5 +1,52 @@
 """Solution of the task."""
 
 
+class Tree:
+    def __init__(self, tiers: int):
+        self._data = self._create_tree_data(tiers)
+
+    @property
+    def width(self) -> int:
+        return len(self._data[0])
+
+    @property
+    def height(self) -> int:
+        return len(self._data)
+
+    @property
+    def data(self) -> list[str]:
+        return self._data
+
+    def append_air_lines(self, amount: int):
+        if not amount:
+            return
+
+        new_data: list[str] = [' ' * self.width] * amount
+        new_data.extend(self._data)
+        self._data = new_data
+
+    @staticmethod
+    def _create_tree_data(tiers: int) -> list[str]:
+        data: list[str] = []
+
+        stars = ''
+        for i in range(0, tiers + 1):
+            stars = stars + '**' if stars else '*'
+            data.append(stars)
+
+        max_stars_count = len(data[-1])
+        data = [f'{line: ^{max_stars_count}}' for line in data]
+        return data
+
+
 def print_forest(*args: int) -> str:
-    return ', '.join(map(str, args))
+    trees = [Tree(tiers) for tiers in args]
+    max_height = max(trees, key=lambda item: item.height).height
+    for tree in trees:
+        tree.append_air_lines(max_height - tree.height)
+
+    image_rows: list[str] = []
+    for i in range(0, max_height):
+        image_rows.append(''.join(tree.data[i] for tree in trees))
+
+    return '\n'.join(image_rows)
